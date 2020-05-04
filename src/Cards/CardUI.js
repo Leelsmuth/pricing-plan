f68e28eea23964d95b5e23791d669a0d06d691ab
Leelsmuth <femianjorin@rocketmail.com>
2020-05-04 14:02:40 +0100

    some functionaility

--- src/Cards/CardUI.js
+++ src/Cards/CardUI.js
@@ -5,17 +5,19 @@ import './card.style.css'
 const Card = props => {
 
   return (
-    <div className='card text-center'>
-      <div className="overflow">
-        <img src={Img1} alt="Image1" className="card-img-top" />
-      </div>
+    <div className="col-md-4">
+      <div className='card text-center'>
+        <div className="overflow">
+          <img src={Img1} alt="Image1" className="card-img-top" />
+        </div>
 
-      <div className='card-body text-dark'>
-        <h4 className='card-title'> Card Plan </h4>
-        <p className='card-text text-secondary'>Lorem Ipsum</p>
-        <a href='#' className='btn btn-outline-success'>Go Anywhere</a>
-      </div>
+        <div className='card-body text-dark'>
+          <h4 className='card-title'>{props.product.title}</h4>
+          <p className='card-text text-secondary'>Lorem Ipsum</p>
+          <a href='#' className='btn btn-outline-success'>Go Anywhere</a>
+        </div>
 
+      </div>
     </div>
   );
 }
--- src/Cards/Cards.js
+++ src/Cards/Cards.js
@@ -1,15 +1,18 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Card from './CardUI'
+import data from '../data'
 
 function Cards(props) {
-  // const[]
+  const [products] = useState(data);
 
   return (
     <div className="container-fluid d-flex justify-content-center">
       <div className="row">
-        <div className="col-md-4"><Card /></div>
-        <div className="col-md-4"><Card /></div>
-        <div className="col-md-4"><Card /></div>
+        {/* <div className="col-md-4"> */}
+          {products.map(product => (
+            <Card key={product.id} product={product} />
+          ))}
+        {/* </div> */}
       </div>
     </div>
   )
